c57d1b76cabed86ccd37585a2ad4c9e0a8e4bcc2
Duc Pham <duc.pham@alithis.com>
2013-04-22 15:06:24 -0400

    First time using Google API - geocode

--- NoteTaLoc-web/NoteTaLoc/Controllers/SaisiNoteController.cs
+++ NoteTaLoc-web/NoteTaLoc/Controllers/SaisiNoteController.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace NoteTaLoc.Controllers
 {
@@ -15,10 +18,9 @@ namespace NoteTaLoc.Controllers
 
         public ActionResult Index()
         {
-            SaisiNoteForm form = new SaisiNoteForm();
-            form.Pays = "Canada";
+            //SaisiNoteForm form = new SaisiNoteForm();
 
-            return View(form);
+            return View();
         }
 
         //
@@ -29,8 +31,56 @@ namespace NoteTaLoc.Controllers
         {
             if (ModelState.IsValid)
             {
-                ViewBag.Message = "La note a été enregistrée.";
-                ViewBag.NumTimes = 1;
+                var address = ConcatenationAddress(form);
+                string url = "http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false";
+                var urlToValidate = string.Format(url, address);
+                WebResponse response = null;
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.Method = "GET";
+                    response = request.GetResponse();
+                    if (Response != null)
+                    {
+                        XPathDocument document = new XPathDocument(response.GetResponseStream());
+                        XPathNavigator navigator = document.CreateNavigator();
+                        navigator.MoveToChild("GeocodeResponse", "");
+                        navigator.MoveToChild("status", "");
+                        XmlReader status = navigator.ReadSubtree();
+                        String st = "";
+                        while (status.Read())
+                        {
+                            st = status.ReadInnerXml();
+                        }
+                        if (st != "OK")
+                        {
+                            ViewBag.Message = "vous devez inserez une adresse valide!";
+                            ViewBag.NumTimes = 1;
+                        }
+                        else
+                        {
+                            XPathNodeIterator result = navigator.Select("/GeocodeResponse/result");
+                            //XPathNodeIterator formattedAddressIterator = result.Current.Select("formatted_address");
+                            XPathNodeIterator geometryIterator = result.Current.Select("geometry");
+                            XPathNodeIterator locationIterator = geometryIterator.Current.Select("location");
+                            XPathNodeIterator latIterator = locationIterator.Current.Select("lat");
+                            XPathNodeIterator lngIterator = locationIterator.Current.Select("lng");
+                            ViewBag.Message = "La note a été enregistrée.";
+                            ViewBag.NumTimes = 1;
+                        }
+                    }
+                        else
+                        {
+                            ViewBag.Message = "Aucune réponse du serveur!";
+                            ViewBag.NumTimes = 1;
+                        }
+
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "La note n'a pas été enregistrée. Réessayer plus tard!";
+                    ViewBag.NumTimes = 1;
+                }
             }
             return View(form);
         }
@@ -40,9 +90,37 @@ namespace NoteTaLoc.Controllers
         [HttpPost]
         public ActionResult Noter(SaisiNoteForm form)
         {
-            ViewBag.Message="La note a été enregistrée.";
+            ViewBag.Message = "La note a été enregistrée.";
             ViewBag.NumTimes = 1;
             return View(form);
+        }
+
+        private string ConcatenationAddress(SaisiNoteForm form)
+        {
+            string address = "";
+            if (form.Numero != null)
+                address += form.Numero;
+            addField(address, form.Rue);
+            addField(address, form.Rue);
+            addField(address, form.Appartement);
+            addField(address, form.Localite);
+            addField(address, form.CodePostal);
+            addField(address, form.Region);
+            addField(address, form.Pays);
+
+            return address;
+        }
+
+
+        private void addField(String src, String field) {
+            if (String.IsNullOrEmpty(field))
+            {
+                if (String.IsNullOrEmpty(src) && src[src.Length-1] != '+')
+                {
+                    src += "+";
+                }
+                src += field;
+            }
         }
     }
 }
--- NoteTaLoc-web/NoteTaLoc/Models/SaisiNoteForm.cs
+++ NoteTaLoc-web/NoteTaLoc/Models/SaisiNoteForm.cs
@@ -8,18 +8,19 @@ namespace NoteTaLoc.Models
 {
     public class SaisiNoteForm
     {
-        [Required(ErrorMessage = "Le numéro est obligatoire.")]
-        public int Numero { get; set; }
-
+        //[Required(ErrorMessage = "Le numéro est obligatoire.")]
+        public int  Numero { get; set; }
+        //[Required(ErrorMessage = "La rue est obligatoire.")]
         public string Rue { get; set; }
         public string Appartement { get; set; }
         public string Localite { get; set; }
+        //[Required(ErrorMessage = "Le code postal est obligatoire.")]
         public string CodePostal { get; set; }
         public string Region { get; set; }
 
         public string Pays { get; set; }
-        [Required(ErrorMessage = "Le numéro est obligatoire.")]
-        [Range(0, 5, ErrorMessage = "Note must be between 0 and 5")]
+        //[Required(ErrorMessage = "Le numéro est obligatoire.")]
+        //[Range(0, 5, ErrorMessage = "La note doit être comprise entre 0 et 5")]
         public int Note { get; set; }
 
 
