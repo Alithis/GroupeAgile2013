5ed4b5ddb2a2d3a61fe9a24d6acbe552f9d58930
Sunny <sunny.hum@alithis.com>
2013-04-29 17:10:41 -0400

    added registration and login

--- NoteTaLoc-web/NoteTaLoc/Models/AccountModels.cs
+++ NoteTaLoc-web/NoteTaLoc/Models/AccountModels.cs
@@ -5,14 +5,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Globalization;
 using System.Web.Security;
+using Recaptcha;
+using Recaptcha.Design;
 
 namespace NoteTaLoc.Models
 {
     public class RegisterModel
     {
         [Required]
+        [Display(Name = "Last name")]
+        public string UserLName { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string UserFName { get; set; }
+
+        [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
@@ -24,5 +39,25 @@ namespace NoteTaLoc.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public Boolean TermAndConditions { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class UserLoggedInModel
+    {
+        public string LoggedInUserName { get; set; }
     }
 }
