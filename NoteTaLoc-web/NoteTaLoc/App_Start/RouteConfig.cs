e9c81dc85ff442396c55ccd99de22aadf84383e7
bigcitylover <arnaud.nemrod@gmail.com>
2013-05-23 15:33:58 -0400

    ARN modification de l'interface

--- NoteTaLoc-web/NoteTaLoc/App_Start/RouteConfig.cs
+++ NoteTaLoc-web/NoteTaLoc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NoteTaLoc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AdresseTable", action = "SearchNoted", id = UrlParameter.Optional }
             );
         }
     }
