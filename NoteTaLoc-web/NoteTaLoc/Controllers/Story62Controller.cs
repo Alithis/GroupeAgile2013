d002bfc8192f671c4abeba2d806a593284c8153c
delemarchand <delemarchand@gmail.com>
2013-06-16 18:05:22 -0400

    spécifique pour réaliser la story62 - à des fins de test uniquement

---
+++ NoteTaLoc-web/NoteTaLoc/Controllers/Story62Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoteTaLoc.Controllers
+{
+    public class Story62Controller : Controller
+    {
+        //
+        // GET: /Story62/
+
+        public ActionResult Index(string idNote)
+        {
+            ViewBag.idNote = "Story 62 : " + idNote;
+            return View();
+        }
+
+    }
+}
