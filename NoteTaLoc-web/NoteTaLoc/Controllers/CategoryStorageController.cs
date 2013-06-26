b3b37ae2c30f216e968b664817b764b5eacc528d
Duc Pham <duc.pham@alithis.com>
2013-06-26 19:16:07 -0400

    ajax criterias

---
+++ NoteTaLoc-web/NoteTaLoc/Controllers/CategoryStorageController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NoteTaLoc.Utilitary;
+
+namespace NoteTaLoc.Controllers
+{
+    public class CategoryStorageController : Controller
+    {
+
+        NoteTaLoc.Utilitary.CategoryStorage categoryStorage;
+
+        public CategoryStorageController()
+        {
+            categoryStorage = (NoteTaLoc.Utilitary.CategoryStorage) System.Web.HttpContext.Current.Application["Categories"];
+        }
+
+        public JsonResult GetCategory(String language)
+        {
+            JsonResult result = new JsonResult();
+            result.Data = categoryStorage.getCategoryListJSON(language);
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
+        }
+
+        public JsonResult GetCriteria(String language)
+        {
+            JsonResult result = new JsonResult();
+            result.Data = categoryStorage.getCriteriaListJSON(language);
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
+        }
+
+    }
+}
