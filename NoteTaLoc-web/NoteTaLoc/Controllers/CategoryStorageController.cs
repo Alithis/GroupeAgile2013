587bb6abeec4f1c11cb72dc24397ac92f73a6674
bigcitylover <arnaud.nemrod@gmail.com>
2013-06-26 18:35:54 -0400

    nouveau service pour appel ajax

---
+++ NoteTaLoc-web/NoteTaLoc/Controllers/CategoryStorageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoteTaLoc.Controllers
+{
+    public class CategoryStorageController : Controller
+    {
+
+        NoteTaLoc.Utilitary.CategoryStorage categoryStorage;
+
+        public CategoryStorageController()
+        {
+            categoryStorage = new Utilitary.CategoryStorage();
+        }
+
+        public JsonResult GetCategory(String language)
+        {
+            return categoryStorage.getCategoryListJSON(language);
+        }
+
+        public JsonResult GetCriteria(String language)
+        {
+            return categoryStorage.getCriteriaListJSON(language);
+        }
+
+    }
+}
